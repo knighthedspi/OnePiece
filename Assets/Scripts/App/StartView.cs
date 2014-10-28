badcdfb7eccf14cb9d9c745e56dc9ad4a4113be3
knight <knighthedspi@gmail.com>
2014-10-28 18:00:31 +0900

    add gameplay view

---
+++ Assets/Scripts/App/StartView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartView : View {
+
+	public GameObject StartBtn;
+
+	[HideInInspector]
+	public UI 		  UI {get; private set;}
+
+	// init in awake funtion
+	void Awake(){
+		UI = gameObject.AddComponent<UI>();
+		Debug.Log ("add UI class");
+		UI.AttachButton(StartBtn, onStartBtnClicked);
+	}
+
+	private void onStartBtnClicked(){
+		Debug.Log ("start loading game");
+		var viewNames = new Dictionary<string, object[]>();
+		viewNames.Add("GamePlay",null);
+		ViewLoader.Instance.CleanLoad(viewNames);
+	}
+}
