87bd0a4e8a5f011eece018afb67068eccd023bed
knight <knighthedspi@gmail.com>
2014-11-04 12:46:48 +0900

    update auto hint

--- Assets/Scripts/App/GamePlay/GamePlay.cs
+++ Assets/Scripts/App/GamePlay/GamePlay.cs
@@ -8,16 +8,12 @@ public class GamePlay : GameSystem_LinkMatch {
 
 	private List<List<Block>> list_matches;
 	
-	private Dictionary<List<Block> , List<Vector2>> dict;
-
-
 
 	// Use this for initialization
 	public override void Start () {
 		base.Start();
 		visited = new bool[(int) _tilesNum.x, (int)_tilesNum.y];
 		list_matches = new List<List<Block>>();
-		dict = new Dictionary< List<Block>, List<Vector2> >();
 	}
 
 	// find hint
@@ -32,56 +28,48 @@ public class GamePlay : GameSystem_LinkMatch {
 				_hints = matchingSet;
 		}
 
-		Debug.Log("size of line : " + _hints.Count);
-
-		List<Vector2> result = dict[_hints];
-		foreach(Vector2 point in result){
-			Debug.Log("point : " + point.x + ";" + point.y); 
-		}
-
 	}
 
 	private bool checkIsSameTypeOfBlock(Block b1, Block b2){
 		return (b1._type == b2._type);
 	}
 
-	private void Search(int x, int y, Block type, List<Block> matchingSet, List<Vector2> resultSet){
+	private void Search(int x, int y, Block type, List<Block> matchingSet ){
 
 		if(checkIsSameTypeOfBlock(_tiles[x,y], type)){
 			matchingSet.Add(_tiles[x,y]);
-			resultSet.Add(new Vector2(x,y));
 			visited[x,y] = true;
 			if( x < _tilesNum.x - 1 && !visited[x + 1, y]){
-				Search(x + 1, y, type, matchingSet, resultSet);
+				Search(x + 1, y, type, matchingSet);
 			}
 
 			if( y < _tilesNum.y - 1 && !visited[x, y + 1]){
-				Search(x , y + 1, type, matchingSet, resultSet);
+				Search(x , y + 1, type, matchingSet);
 			}
 
 			if( x > 0 && !visited[x - 1, y]){
-				Search(x - 1, y, type, matchingSet, resultSet);
+				Search(x - 1, y, type, matchingSet);
 			}
 			
 			if( y > 0 && !visited[x, y - 1]){
-				Search(x , y - 1, type, matchingSet, resultSet);
+				Search(x , y - 1, type, matchingSet);
 			}
 
 			if( x % 2 != 0) {
 				if( x < _tilesNum.x - 1 && y > 0  && !visited[x + 1, y - 1]){
-					Search(x + 1, y - 1, type, matchingSet, resultSet);
+					Search(x + 1, y - 1, type, matchingSet);
 				}
 
 				if( x > 0 && y > 0  && !visited[x - 1, y - 1]){
-					Search(x - 1, y - 1, type, matchingSet, resultSet);
+					Search(x - 1, y - 1, type, matchingSet);
 				}
 			}else{
 				if( x < _tilesNum.x - 1 && y < _tilesNum.y - 1  && !visited[x + 1, y + 1]){
-					Search(x + 1, y + 1, type, matchingSet, resultSet);
+					Search(x + 1, y + 1, type, matchingSet);
 				}
 				
 				if( x > 0 && y < _tilesNum.y - 1  && !visited[x - 1, y + 1]){
-					Search(x - 1, y + 1, type, matchingSet, resultSet);
+					Search(x - 1, y + 1, type, matchingSet);
 				}
 			}
 		}
@@ -105,11 +93,9 @@ public class GamePlay : GameSystem_LinkMatch {
 				if(!visited[x,y]){
 					visited[x, y] = true;
 					List<Block> matchingSet = new List<Block>();
-					List<Vector2> resultSet = new List<Vector2>();
-					Search(x, y, _tiles[x,y], matchingSet, resultSet);
+					Search(x, y, _tiles[x,y], matchingSet);
 					if(matchingSet.Count > 2){
 						list_matches.Add(matchingSet);
-						dict.Add(matchingSet, resultSet);
 					}
 				}
 			}
