5596052b82ba68d20a417f82c962586a77f06372
knight <knighthedspi@gmail.com>
2014-11-04 20:58:53 +0900

    change view in gameplay

--- Assets/Scripts/App/GamePlay/GamePlay.cs
+++ Assets/Scripts/App/GamePlay/GamePlay.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public class GamePlay : GameSystem_LinkMatch {
 
+	public int deltaStartX  = 13;
+	public int deltaStartY  = -20;
+
 	private bool [,] visited ;
 
 	private List<List<Block>> list_matches;
@@ -28,6 +31,27 @@ public class GamePlay : GameSystem_LinkMatch {
 				_hints = matchingSet;
 		}
 
+	}
+
+	// tile pos
+	public override Vector2 tilePos(int x,int y){
+		if(x >= (int)_tilesNum.x || x < 0)return new Vector2(0,0);
+		if(y >= (int)_tilesNum.y || y < 0)return new Vector2(0,0);
+		
+		float curve = x%2*40;
+		
+		Transform trans = _board.transform;
+		
+		float width = _board.GetComponent<UISprite>().width;
+		float height = _board.GetComponent<UISprite>().height;
+		float startX = trans.localPosition.x - width/2 + deltaStartX;
+		float startY = trans.localPosition.y - height/2 + deltaStartY;
+		float posX = (x*_tileSize.x);
+		float posY = ((_tilesNum.y-y-1)*(_tileSize.y));
+		return new Vector2(
+			posX + startX + _tileSize.x/2 + _boardPadding.x + _tilesMargin.x * x,
+			posY + startY + _tileSize.y/2 + _boardPadding.y + _tilesMargin.y * y + curve
+			);
 	}
 
 	private bool checkIsSameTypeOfBlock(Block b1, Block b2){
