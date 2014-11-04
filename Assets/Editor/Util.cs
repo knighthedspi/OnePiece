c1046d217e0f6a2de4494da3b33a27a4a51c67d6
knight <knighthedspi@gmail.com>
2014-11-04 17:04:08 +0900

    add save prefab in editor mode

---
+++ Assets/Editor/Util.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class Util : Editor {
+	#if UNITY_EDITOR
+	[UnityEditor.MenuItem("Edit/SavePrefab %&s")]
+	static void SavePrefab(){
+		AssetDatabase.SaveAssets();
+	}
+	#endif
+}
