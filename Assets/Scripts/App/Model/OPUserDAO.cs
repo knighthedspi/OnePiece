2070fd25465d2d73f99829480704040b2b13cf81
unglevan <huythang0312@gmail.com>
2014-11-14 18:50:15 +0900

    add user service add test code (test code not working)

--- Assets/Scripts/App/Model/OPUserDAO.cs
+++ Assets/Scripts/App/Model/OPUserDAO.cs
@@ -4,77 +4,85 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class OPUserDAO : OPUser{
+public class OPUserDAO : OPUser
+{
 
-	private static string tableName = "OPUser";
-	private static OPUserDAO sInstance;
-	private static SQLiteDB db;
+    private static string tableName = "OPUser";
+    private static OPUserDAO sInstance;
+    private static SQLiteDB db;
+    private const int MAX_HEALTH = 5;
 
-	private const int MAX_HEALTH = 5;
+    public static OPUserDAO Instance {
+        get {
+            if(sInstance == null) {
+                sInstance = new OPUserDAO();
+                db = DBManager.UserDb;
+                if(!_isTableExist()) {
+                    Debug.LogError("table not exist");
+                    initUser();
+                }
+            }
+            return sInstance;
+        }
+    }
 
-	public static OPUserDAO Instance {
-		get {
-			if(sInstance == null){
-				sInstance = new OPUserDAO();
-				db = DBManager.UserDb;
-				if(!_isTableExist())
-				{
-					Debug.LogError("table not exist");
-					initUser();
-				}
-			}
-			return sInstance;
-		}
-	}
 
-	public OPUser getUserWithCondition()
-	{
+    public OPUser getCurrentUser()
+    {
 //		OPUser user = new OPUser();
 //		QueryCondition<OPUser> condition = new QueryCondition<OPUser>();
 //		var list = new List<OPUser>(){user};
 //		condition.WithEqualConditions(list);
 //		condition.Limit = 1;
 //		Debug.LogError("select user");
-		string query = "select * from OPUser limit 1";
-		List<OPUser> users = GenericDao<OPUser>.Instance.Get(db, query);
-		if(users.Count < 0)
-		{
-			throw new EntryPointNotFoundException("user not found");
-		}
-		return users[0];
-	}
+        string query = "select * from OPUser limit 1";
+        List<OPUser> users = GenericDao<OPUser>.Instance.Get(db, query);
+        if(users.Count < 0) {
+            throw new EntryPointNotFoundException("user not found");
+        }
+        return users[0];
+    }
 
-	/// <summary>
-	/// create user
-	/// </summary>
-	private static void initUser()
-	{
-		//Create table
-		createTable();
-		//create user
-		OPUser user = new OPUser();
-		user.UserName = "luffy"; //#TODO random some meaning name with each language
-		user.Exp = 0;
-		user.LevelId = 1;
-		user.Health = MAX_HEALTH;
-		user.Score = 0;
-		user.HighScore = 0;
-		user.CurrentMonsterID = 0;
-		user.CreatedAt = TimeStampUtility.convertTimeToInt(DateTime.UtcNow);
-		user.UpdatedAt = TimeStampUtility.convertTimeToInt(DateTime.UtcNow);
-		Debug.LogError("create user");
-		GenericDao<OPUser>.Instance.Put(db, user);
-	}
+    public bool save(OPUser user)
+    {
+        try {
+            GenericDao<OPUser>.Instance.Update(db, user, new QueryCondition<OPUser>());
+            return true;
+        } catch(Exception ex) {
+            return false;
+        }
+    }
 
-	private static void createTable()
-	{
-		GenericDao<OPUser>.Instance.Create(db);
-	}
+    /// <summary>
+    /// create user
+    /// </summary>
+    private static void initUser()
+    {
+        //Create table
+        createTable();
+        //create user
+        OPUser user = new OPUser();
+        user.UserName = "luffy"; //#TODO random some meaning name with each language
+        user.Exp = 0;
+        user.LevelId = 1;
+        user.Health = MAX_HEALTH;
+        user.Score = 0;
+        user.HighScore = 0;
+        user.CurrentMonsterID = 0;
+        user.CreatedAt = TimeStampUtility.convertTimeToInt(DateTime.UtcNow);
+        user.UpdatedAt = TimeStampUtility.convertTimeToInt(DateTime.UtcNow);
+        GenericDao<OPUser>.Instance.Put(db, user);
+    }
 
-	private static bool _isTableExist()
-	{
+    private static void createTable()
+    {
+        GenericDao<OPUser>.Instance.Create(db);
+    }
+
+    private static bool _isTableExist()
+    {
 //		string query = "select name from sqlite_master where type='table' and name = " + tableName;
-		//#TODO must implement right way
-		return true;
-	}
+        //#TODO must implement right way
+        return true;
+    }
 }
