e23b12cfc96dd2f01831b65f7eb8dc028bd8bbb0
unglevan <huythang0312@gmail.com>
2014-11-14 18:46:04 +0900

    add AppManager

---
+++ Assets/Scripts/App/Manager/AppManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppManager : Singleton<AppManager>
+{
+    public static OPUser user;    
+
+    void Awake()
+    {
+        user = OPUserDAO.Instance.getCurrentUser();
+    }
+
+    void OnDestroy()
+    {
+
+    }
+}
