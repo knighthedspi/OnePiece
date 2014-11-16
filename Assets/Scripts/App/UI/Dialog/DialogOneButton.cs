07e90f08d75af1f4a6e118f6b15cba84139d61d6
sukoshi <duongminhkhoa@Duongs-MacBook-Pro.local>
2014-11-16 16:50:21 +0900

    add dialog module

---
+++ Assets/Scripts/App/UI/Dialog/DialogOneButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogOneButton : DialogBase {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void InitUI ()
+	{
+		base.InitUI ();
+	}
+
+	public static void Create(string content, EventDelegate.Callback callBack,string title= "", string button_label = "")
+	{
+		DialogData dialogData = new DialogData ();
+		dialogData.dialogType = DialogType.DialogOneButton;
+		dialogData.textData.Add("content", content);
+		dialogData.textData.Add ("title",title);
+		dialogData.eventData.Add ("btnCenter", callBack);
+		if(!string.IsNullOrEmpty(button_label))
+			dialogData.textData.Add ("button_label_btnCenter",button_label);
+		DialogManager.Instance.OpenDialog (dialogData, true);
+	}
+}
---
+++ Assets/Scripts/Core/Manager/DialogManager.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogManager : Singleton<DialogManager> {
+
+	private bool _isProcessing;
+	private GameObject _dialogWindow;
+	private UIRoot _uiRoot;
+
+	private DialogBase _currentDialog; 
+	private List<DialogData> _dialogQueue = new List<DialogData>();
+
+	void Awake()
+	{
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// Complete this instance.
+	/// </summary>
+	public void Complete()
+	{
+		_isProcessing = false;
+		Close ();
+	}
+
+	/// <summary>
+	/// Opens the dialog.
+	/// </summary>
+	/// <param name="dialogData">Dialog data.</param>
+	/// <param name="immediate">If set to <c>true</c> immediate.</param>
+	public void OpenDialog(DialogData dialogData, bool immediate)
+	{
+		if(immediate)
+		{
+			CreateDialog(dialogData);
+		}
+		else
+		{
+			_dialogQueue.Add(dialogData);
+			ProcessDialog();
+		}
+	}
+
+
+	private void Close()
+	{
+		if(_currentDialog)
+		{
+			_currentDialog.Close();
+		}
+		ProcessDialog ();
+	}
+
+	private void ProcessDialog()
+	{
+		if (_isProcessing)
+						return;
+		if (_dialogQueue.Count <= 0)
+						return;
+		CreateDialog (_dialogQueue [0]);
+		_dialogQueue.RemoveAt (0);
+	}
+
+	private void CreateDialog(DialogData dialogData)
+	{
+		_isProcessing = true;
+		string path = string.Format ("DialogPrefabs/{0}", dialogData.dialogType.ToString ());
+		GameObject dialogPrb = Resources.Load (path, typeof(GameObject)) as GameObject;
+		GameObject parent = GetDialogWindow ();
+		GameObject go = NGUITools.AddChild (parent, dialogPrb);
+		NGUITools.BringForward (go);
+
+		_currentDialog = go.GetComponent<DialogBase> ();
+		_currentDialog.Init (dialogData);
+
+	}
+
+	private GameObject GetDialogWindow()
+	{
+		_uiRoot = GameObject.Find ("main_scene").GetComponentInChildren<UIRoot> ();
+		if(!_dialogWindow)
+		{
+			_dialogWindow = NGUITools.AddChild(_uiRoot.gameObject);
+			_dialogWindow.name = "DialogWindow";
+		}
+		return _dialogWindow;
+	}
+
+}
