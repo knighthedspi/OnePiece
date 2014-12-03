67478487b25b8c53fd4e68aed549376b64a4ac7d
knight <knighthedspi@gmail.com>
2014-12-03 19:38:01 +0900

    update loading view

--- Assets/Scripts/App/Views/LoadingView.cs
+++ Assets/Scripts/App/Views/LoadingView.cs
@@ -3,32 +3,12 @@ using System.Collections;
 
 public class LoadingView : MonoBehaviour{
 
-	public UISprite loadingProgressBar;
 	
-	public float loadingProgress { get;set;} 
-
-	private Animator _loadingAnimator;
-
-	void Awake(){
-		_loadingAnimator = gameObject.GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		loadingProgressBar.fillAmount = loadingProgress;
-	}
-
-	public void setLoadingProgress(float progress){
-		loadingProgress = progress;
-	}
-
 	public void transitionIn(){
 		this.gameObject.SetActive(true);
-		_loadingAnimator.Play(Config.TRANSITION_IN);
 	}
 
 	public void transitionOut(){
-		_loadingAnimator.Play(Config.TRANSITION_OUT);
 		this.gameObject.SetActive(false);
 	}
 }
