7765a8f48578e1dc28d29b63ad32beb92d7daba6
sukoshi <duongminhkhoa@Duongs-MacBook-Pro.local>
2014-12-04 23:25:46 +0900

    fix image

--- Assets/Scripts/Core/3typePuzzle/Block.cs
+++ Assets/Scripts/Core/3typePuzzle/Block.cs
@@ -140,4 +140,5 @@ public class Block : MonoBehaviour {
 		gameObject.Recycle ();
 	}
 	
+
 }
--- Assets/Scripts/Core/3typePuzzle/SpecialBlock.cs
+++ Assets/Scripts/Core/3typePuzzle/SpecialBlock.cs
@@ -1,7 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SpecialBlock : Block {
+	private bool _isActive;
 
+	public override void Init (BlockType type)
+	{
+		base.Init (type);
+		_isActive = false;
+		string typeStr = blockType.ToString ();
+		// upper the fisrt letter
+//		typeStr = char.ToUpper (typeStr [0] + typeStr.Substring (1));
+		gameObject.AddComponent (typeStr);
+	}
+	public override void TouchDown ()
+	{
+		SendMessage ("StartAnimation");
+	}
+
+	public override void TouchUp ()
+	{
+		base.TouchUp ();
+		_isActive = true;
+	}
+
+	public override void Destroy ()
+	{
+		if(_isActive)
+			base.Destroy ();
+	}
 
 }
